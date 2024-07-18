934217b2282f08f0557f8da7350208746f5ae3eb
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-07-18 10:40:44 +0700

    feat: open cors for fe
    test connect signalR

--- WebGame/TicTacToe/Hubs/ChatHub.cs
+++ WebGame/TicTacToe/Hubs/ChatHub.cs
@@ -4,10 +4,10 @@ namespace TicTacToe.Hubs
 {
     public class ChatHub : Hub
     {
-        //public async Task SendMessage(string user, string message)
-        //{
-        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
-        //}
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
 
         public async Task SendMessageToGroup(string groupName, string user, string message)
         {
--- WebGame/TicTacToe/Program.cs
+++ WebGame/TicTacToe/Program.cs
@@ -5,31 +5,39 @@ using TicTacToe.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+            builder => builder
+                .SetIsOriginAllowed(_ => true) // cho phép tất cả các port
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials());
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.MapHub<ChatHub>("/chatHub");
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-    endpoints.MapHub<ChatHub>("/chathub");
-});
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.UseCors("AllowAllOrigins");
+
 app.Run();
+
